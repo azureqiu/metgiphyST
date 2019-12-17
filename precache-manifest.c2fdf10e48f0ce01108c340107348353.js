591cce54a8042d4cdf8274590603e027ed62f7ff
Qiuji Zhao <qiujizhao@Qiujis-MacBook-Pro.local>
2019-12-17 00:54:25 -0500

    Updates

---
+++ precache-manifest.c2fdf10e48f0ce01108c340107348353.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe29b6b80b06c552a3b2d9b69f67190",
+    "url": "/azureqiu/metgiphyST.io/index.html"
+  },
+  {
+    "revision": "ec912424dc3b6cc05800",
+    "url": "/azureqiu/metgiphyST.io/static/css/main.e6184dde.chunk.css"
+  },
+  {
+    "revision": "1e5328d2dd496dfc7949",
+    "url": "/azureqiu/metgiphyST.io/static/js/2.168f3519.chunk.js"
+  },
+  {
+    "revision": "ec912424dc3b6cc05800",
+    "url": "/azureqiu/metgiphyST.io/static/js/main.240e60d6.chunk.js"
+  },
+  {
+    "revision": "4f21a1a9e69e1dedcf3c",
+    "url": "/azureqiu/metgiphyST.io/static/js/runtime-main.89075d56.js"
+  }
+]);
